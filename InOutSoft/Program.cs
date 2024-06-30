ef86553f4643da48ebebdcaf1c19a4b0157b0d55
Luis Ricardo Luna <47165717+luisrlunaa@users.noreply.github.com>
2024-06-30 10:36:30 -0400

    Add project files.

---
+++ InOutSoft/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace InOutSoft
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new HomeForm());
+        }
+
+
+        public static string connectionString;
+
+        public static SqlConnection conection()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        public static void Conectar()
+        {
+            if (conection().State == ConnectionState.Closed)
+                conection().Open();
+        }
+
+        public static void Desconectar()
+        {
+            if (conection().State == ConnectionState.Open)
+                conection().Close();
+        }
+    }
+}
